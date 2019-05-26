cff517291732204f83fac5800417c279f3328cd0
mitchnm <mitchndungu21@gmail.com>
2019-05-26 16:50:30 +0300

    made 1st migration

--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
 from app import create_app, db
 from flask_script import Manager
 from app.models import User, Role
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app('test')
 
 manager = Manager(app)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
