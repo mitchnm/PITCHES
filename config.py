ff5ed8e2fce2d7b661f53d92ed03ed67975806bd
mitchnm <mitchndungu21@gmail.com>
2019-05-26 16:45:19 +0300

    fixed flask errors

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mitch:qwertyuiop@localhost/pitches'
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test':TestConfig
+}
---
+++ manage.py
@@ -0,0 +1,16 @@
+from app import create_app, db
+from flask_script import Manager
+from app.models import User, Role
+
+
+app = create_app('test')
+
+manager = Manager(app)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+if __name__ == '__main__':
+    manager.run()
