760f3aa719b78dbd737d44d575ffc0a8278032ef
mitchnm <mitchndungu21@gmail.com>
2019-05-29 13:02:29 +0300

    added gunicorn

--- manage.py
+++ manage.py
@@ -20,7 +20,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Pitch=Pitch, Comments=Comments)
+    return dict(app=app, db=db, User=User)
 
 
 if __name__ == '__main__':
